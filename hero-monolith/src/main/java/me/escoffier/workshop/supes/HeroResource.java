4316247d3af21c0252c2eb908dbf6b119034b027
boukrisw <boukrisw@gmail.com>
2022-01-19 21:14:24 +0100

    Self-repair hero and villain services

--- hero-monolith/src/main/java/me/escoffier/workshop/supes/HeroResource.java
+++ hero-monolith/src/main/java/me/escoffier/workshop/supes/HeroResource.java
@@ -1,7 +1,9 @@
 package me.escoffier.workshop.supes;
 
+import io.vertx.core.Vertx;
 import org.jboss.logging.Logger;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,6 +13,9 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class HeroResource {
 
+    @Inject
+    Vertx vertx;
+
     private static final Logger LOGGER = Logger.getLogger(HeroResource.class);
 
     @GET
@@ -19,5 +24,24 @@ public class HeroResource {
         Hero hero = Hero.findRandom();
         LOGGER.debug("Found random hero " + hero);
         return hero;
+    }
+
+    /**
+     * Explicitly crashes the service.
+     */
+    @GET
+    @Path("/crash")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String crash() {
+        LOGGER.info("Dying in ~1 second");
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            vertx.close();
+        }).start();
+        return "bye bye Hero";
     }
 }
--- villain-monolith/src/main/java/me/escoffier/workshop/supes/VillainResource.java
+++ villain-monolith/src/main/java/me/escoffier/workshop/supes/VillainResource.java
@@ -1,7 +1,9 @@
 package me.escoffier.workshop.supes;
 
+import io.vertx.core.Vertx;
 import org.jboss.logging.Logger;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,6 +13,9 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class VillainResource {
 
+    @Inject
+    Vertx vertx;
+
     private static final Logger LOGGER = Logger.getLogger(VillainResource.class);
 
     @GET
@@ -19,5 +24,24 @@ public class VillainResource {
         Villain villain = Villain.findRandom();
         LOGGER.debug("Found random villain " + villain);
         return villain;
+    }
+
+    /**
+     * Explicitly crashes the service.
+     */
+    @GET
+    @Path("/crash")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String crash() {
+        LOGGER.info("Dying in ~1 second");
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            vertx.close();
+        }).start();
+        return "bye bye Villain";
     }
 }
